db84f04caea6e59131ca75100f9b80dfe885be63
Akmal <akmalbekortiqovv@gmail.com>
2023-06-24 03:08:14 +0500

    create home sections

--- src/controllers/controllers.ts
+++ src/controllers/controllers.ts
@@ -1,8 +1,44 @@
-import { Request, Response , NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 
 export function getIndex(req: Request, res: Response) {
-   res.render("partials/index", {
-     title: "Mind Up",
-     onAdmin: true
-   });
+  res.render("partials/index", {
+    title: "Mind Up",
+    onIndex: true,
+  });
+}
+export function getLogin(req: Request, res: Response) {
+  res.render("auth/register", {
+    title: "Login",
+    loginOn:true
+  });
+}
+export function getTest(req: Request, res: Response) {
+  res.render("Home/quiz", {
+    title: "Mind Up",
+    quizOn :true
+  });
+}
+export function getResult(req: Request, res: Response) {
+  res.render("Home/result", {
+    title: "Mind Up",
+    resultOn :true
+  });
+}
+export function getSetting(req: Request, res: Response) {
+  res.render("Home/setting", {
+    title: "Mind Up",
+    settingOn :true
+  });
+}
+export function getBalls(req: Request, res: Response) {
+  res.render("Home/balls", {
+    title: "Mind Up",
+    ballsOn :true
+  });
+}
+export function getBlog(req: Request, res: Response) {
+  res.render("Home/blog", {
+    title: "Mind Up",
+    blogOn :true
+  });
 }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,15 @@
 import { Router, request } from "express";
-import { getIndex } from "../controllers/controllers";
+import { getBalls, getBlog, getIndex, getLogin , getResult, getSetting, getTest } from "../controllers/controllers";
 
 const router = Router();
 
 router.get('/', getIndex ) 
+router.get('/quiz', getTest)
+router.get('/balls', getBalls)
+router.get('/result', getResult)
+router.get('/setting', getSetting)
+router.get('/blog', getBlog)
+
+router.get('/login', getLogin)
 
 export default router;
